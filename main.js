fb025613995914f6a08d236eb362123dd48c09c2
shahinalom <shahinalom3511371@gmail.com>
2024-07-05 00:44:38 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('toggle-dark-mode').addEventListener('click', function() {
+      document.body.classList.toggle('dark-mode');
+  });
+  
+// document.getElementById('toggle-dark-mode').addEventListener('click', function(){
+//       document.body.classList.toggle('dark-mode')
+// })  
